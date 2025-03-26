45d324a9717403b913264dcf8fa0e90facaa6026
jeswin9567 <jeswinmathew9567@gmail.com>
2025-03-26 22:14:13 +0530

    timetable css

--- frontend/src/components/manager/Teacher/Timetable.jsx
+++ frontend/src/components/manager/Teacher/Timetable.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './TeacherTimetable.css';
+import './Timetable.css';
 import * as XLSX from 'xlsx';
 
 const TeacherTimetable = () => {
